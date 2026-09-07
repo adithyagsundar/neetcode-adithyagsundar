8dc30f559472a39c4a0b31723ab80f585c7f70b2
Adithya Gnanasundar <147122594+adithyagsundar@users.noreply.github.com>
2026-09-07 09:40:24 -0700

    Add: string-encode-and-decode - submission-23

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-23.py
@@ -0,0 +1,36 @@
+class Solution:
+    def encode(self, strs: List[str]) -> str:
+        """
+        5#string
+        """
+
+        res = ""
+
+        for s in strs:
+            res += str(len(s)) + "#" + s
+        return res
+
+    def decode(self, s: str) -> List[str]:
+        """
+        two pointers, i and j
+        i is at the start of the string and j goes through the string
+        use the length of the string (first charcter) to know how far to append to the result list
+        """
+        
+        res = []
+
+        i = 0
+
+        while i < len(s):
+            j = i
+            while s[j] != "#":
+                j += 1
+            length = int(s[i])
+            res.append(s[j + 1 : j + 1 + length])
+            i = j + 1 + length
+        return res
+
+
+
+
+
