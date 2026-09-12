c402fb28dc81ea8e5cc53becb4fe07714bcc1d53
Adithya Gnanasundar <147122594+adithyagsundar@users.noreply.github.com>
2026-09-12 09:22:23 -0700

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        L, R = 0, len(s) - 1
+
+        while L < R:
+            if s[L].lower() != s[R].lower():
+                return False
+            L += 1
+            R -= 1
+        return True
